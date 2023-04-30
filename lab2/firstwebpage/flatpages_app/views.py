8151c9fd42c095c0ecf490904e4f889bb98c8a90
Thomas Edward <th0m45y3@gmail.com>
2023-04-30 17:56:51 +0100

    lab2 done

---
+++ lab2/firstwebpage/flatpages_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# answer without render page
+# def home(request):
+# 	# text = u'Привет, Мир!ja'.encode('utf8')
+# 	text = "hello, world!"
+# 	return HttpResponse(text) #, content_type="text/plain"
+
+def home(request):
+    return render(request, 'index.html')
